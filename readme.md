bdaadc0085811ef2e3f35cd0d64655360af46504
Arfonzo2k18 <alfonsomtb98@gmail.com>
2019-04-07 18:27:14 +0200

    Añadiendo readme.md

---
+++ readme.md
@@ -0,0 +1,25 @@
+# SPICA PROYECT
+### Aplicación web para gestionar contenidos que posteriormente serán consumidos por un cliente. Api REST creada con Expressjs.
+
+#### Para instalar los módulos de node:
+    npm install --save
+
+#### Librerías utilizadas:
+    bcrypt: "^3.0.5",
+    bcryptjs: "^2.4.3",
+    body-parser: "^1.18.3",
+    btoa: "^1.2.1",
+    cors: "^2.8.5",
+    express: "^4.16.4",
+    express-handlebars: "^3.0.2",
+    express-session: "^1.15.6",
+    fs: "0.0.1-security",
+    fs-extra: "^7.0.1",
+    jsonwebtoken: "^8.5.1",
+    lodash: "^4.17.11",
+    morgan: "^1.9.1",
+    multer: "^1.4.1",
+    sequelize: "^5.1.1",
+    sqlite3: "^4.0.6"
+
+#### Sistema de gestión de base de datos: SQLite
--- routes/index.router.js
+++ routes/index.router.js
@@ -3,25 +3,10 @@ const router = express.Router();
 
 const usersController = require('../controllers/usuariosController');
   
-// router.get('/users/:id', (req, res) => {
-//     Usuario.findById(req.params.id, {
-//     attributes: ['id', 'nick']
-//   })
-//   .then(result => res.json(result))
-//   .catch(error => {
-//     res.status(412).json({msg: error.message});
-//   });
-// });
-
-// router.delete('/users/:id', (req, res) => {
-//     Usuario.destroy({where: {id: req.params.id}})
-//     .then(result => res.sendStatus(204))
-//     .catch(error => {
-//       res.status(412).json({msg: error.message});
-//     });
-// });
-
 router.post('/register', usersController.createUser);
 router.post('/login', usersController.login);
+router.post('/users');
+router.post('/users/:id');
+router.delete('/users/:id');
 
 module.exports = router;
